c9a99071cb92c85d0d7186270f9cea70f8fa33fe
rStar <starlove124760@gmail.com>
2022-05-21 10:09:51 +0800

    inline method

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -11,54 +11,6 @@ namespace DependencyInjectionWorkshop.Models
     public class AuthenticationService
     {
         /// <summary>
-        /// Password from DB
-        /// </summary>
-        /// <param name="accountId"></param>
-        /// <returns></returns>
-        public string GetPassword(string accountId)
-        {
-            using (var connection = new SqlConnection("my connection string"))
-            {
-                var password = connection.Query<string>("spGetUserPassword" , new { Id = accountId } ,
-                    commandType : CommandType.StoredProcedure).SingleOrDefault();
-
-                return password;
-            }
-        }
-
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="plainText"></param>
-        /// <returns></returns>
-        public string GetHash(string plainText)
-        {
-            var crypt  = new System.Security.Cryptography.SHA256Managed();
-            var hash   = new StringBuilder();
-            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(plainText));
-            foreach (var theByte in crypto)
-            {
-                hash.Append(theByte.ToString("x2"));
-            }
-
-            return hash.ToString();
-        }
-
-        public string GetOtp(string accountId)
-        {
-            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
-            var response   = httpClient.PostAsJsonAsync("api/otps" , accountId).Result;
-            if (response.IsSuccessStatusCode)
-            {
-                return response.Content.ReadAsAsync<string>().Result;
-            }
-            else
-            {
-                throw new Exception($"web api error, accountId:{accountId}");
-            }
-        }
-
-        /// <summary>
         ///
         /// </summary>
         /// <param name="account"></param>
@@ -67,7 +19,32 @@ namespace DependencyInjectionWorkshop.Models
         /// <returns>IsValid</returns>
         public bool Verify(string account , string password , string otp)
         {
-            return false;
+            string passwordFromDb;
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                var password1 = connection.Query<string>("spGetUserPassword" , new { Id = account } ,
+                    commandType : CommandType.StoredProcedure).SingleOrDefault();
+
+                passwordFromDb = password1;
+            }
+
+            var crypt  = new System.Security.Cryptography.SHA256Managed();
+            var hash   = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+            foreach (var theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+
+            var hashedPassword = hash.ToString();
+
+            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
+            var response   = httpClient.PostAsJsonAsync("api/otps" , otp).Result;
+            if (response.IsSuccessStatusCode) { }
+            else throw new Exception($"web api error, accountId:{otp}");
+
+            var currentOtp = response.Content.ReadAsAsync<string>().Result;
+
         }
     }
 }
